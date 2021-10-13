bbf2432413cbf5711e1c89466afe73f130e12e2e
pulidongz <paogravoso@gmail.com>
2021-10-13 15:35:45 +0800

    carousel + vid

--- newtonagham/src/components/Header.js
+++ newtonagham/src/components/Header.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { Grid, Paper, Typography } from '@mui/material';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import { makeStyles } from '@mui/styles';
 import newtonLogo from '../assets/images/Newton-Agham_CMYK.svg';
 import banner from '../assets/images/banner1.png';
-import bannerVid from '../assets/video/bannervid.mp4';
+import bannerVid from '../assets/video/Newton_VR_3.mp4';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -23,6 +24,12 @@ const useStyles = makeStyles((theme) => ({
 export default function Header() {
     const classes = useStyles();
 
+    var banners = [
+        {
+
+        }
+    ]
+
     return(
         <React.Fragment>
             <Grid>
@@ -31,12 +38,17 @@ export default function Header() {
                 </Paper>
             </Grid>
             <Grid>
-                <Paper className={classes.banner}>
-                    {/* <img src={banner} style={{width: '100%', objectFit: 'cover'}}/> */}
+                <Carousel showArrows={true} showThumbs={false}>
+                <div>
+                    <img src={banner} style={{width: '100%', objectFit: 'cover'}}/>
+                </div>
+                <div>
                     <video width="100%" height="100%" autoPlay loop muted>
                         <source src={bannerVid} type="video/mp4" />
                     </video>
-                </Paper>
+                </div>
+            </Carousel>
+
             </Grid>
         </React.Fragment>
     );
